a5716d09c3a036a8c50882354f57a7a9b32ca064
BeardedPlatypus <mwtegelaers@gmail.com>
2020-09-06 12:31:20 +0200

    Extend documentation and cleanup Viewport.Nucleus

--- FabulousSDL.Viewport.Nucleus/include/FabulousSDL.Viewport.Nucleus/View.h
+++ FabulousSDL.Viewport.Nucleus/include/FabulousSDL.Viewport.Nucleus/View.h
@@ -3,20 +3,47 @@
 
 namespace FabulousSDL::Viewport::Nucleus {
 
+/// <summary>
+/// <see cref="view"/> provides a wrapper around our SDL2 code.
+/// </summary>
 class view {
-public:
-	view() = default;
+public:	
+    /// <summary>
+    /// Creates a new <see cref="view"/>.
+    /// </summary>
+    view() = default;
+	
+    /// <summary>
+    /// Initialises this <see cref="view"/>.
+    /// </summary>
+    /// <remarks>
+    /// This will create a new native window.
+    /// </remarks>
+    void initialise();
+	
+    /// <summary>
+    /// Initialises this view in the provided <paramref name="p_native_window"/>
+    /// </summary>
+    /// <param name="p_native_window">A poitner to the native window.</param>
+    void initialise(const void* p_native_window);
 
-	void initialise();
-	void initialise(const void* p_native_window);
-
-	void update();
-	[[nodiscard]] bool should_quit() const;
+    /// <summary>
+    /// Updates this view by rendering a new frame to its window.
+    /// </summary>
+    void update();
+	
+    /// <summary>
+    /// Get whether this <see cref="view"/> should quit.
+    /// </summary>
+    /// <returns>
+    /// Whether this <see cref="view"/> should quit.
+    /// </returns>
+    [[nodiscard]] bool should_quit() const;
 private:
-	SDL_Window* p_window_ = nullptr;
-	SDL_Renderer* p_renderer_ = nullptr;
+    SDL_Window* p_window_ = nullptr;
+    SDL_Renderer* p_renderer_ = nullptr;
 
-	bool should_quit_ = false;
+    bool should_quit_ = false;
 };
 }
 
