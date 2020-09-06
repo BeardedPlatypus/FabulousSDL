29febd3e66f79ed8e92de9a3e87b96a269ee6bbf
BeardedPlatypus <mwtegelaers@gmail.com>
2020-09-06 12:30:25 +0200

    Extend documentation ViewportRenderer

--- FabulousSDL.WPF/ViewportRenderer.fs
+++ FabulousSDL.WPF/ViewportRenderer.fs
@@ -5,6 +5,10 @@ open Xamarin.Forms.Platform.WPF
 open FabulousSDL
 open FabulousSDL.WPF.Components
 
+/// <summary>
+/// <see cref="ViewportRenderer"/> provides the custom renderer implementation
+/// to render the <see cref="Viewport"/> as a <see cref="ViewportControl"/>.
+/// </summary>
 type ViewportRenderer() =
     inherit ViewRenderer<Viewport, ViewportControl>()
 
@@ -17,7 +21,7 @@ type ViewportRenderer() =
         | _ ->
             do ()
 
-
+// Dummy module to ensure this renderer is exported and picked up by Xamarin.Forms
 module Dummy_ViewPortRenderer = 
     [<assembly: ExportRenderer(typeof<FabulousSDL.Viewport>, typeof<ViewportRenderer>)>]
     do ()
